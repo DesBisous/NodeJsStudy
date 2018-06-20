5758ff56a0c632b51d9aecf39cad187344e1d105
Benson <919624732@qq.com>
2018-06-20 23:48:19 +0800

    修改http.js文件

--- koa/router/module/http.js
+++ koa/router/module/http.js
@@ -1,19 +1,19 @@
 // add url-route:
-var hello =  router.get('/hello/:name', async (ctx, next) => {
+var hello =  async (ctx, next) => {
     var name = ctx.params.name;
     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
+};
 
-var index = router.get('/', async (ctx, next) => {
+var index = async (ctx, next) => {
   ctx.response.body = `<h1>Index</h1>
      <form action="/signin" method="post">
          <p>Name: <input name="name" value="koa"></p>
          <p>Password: <input name="password" type="password"></p>
          <p><input type="submit" value="Submit"></p>
      </form>`;
-});
+};
 
-var signin = router.post('/signin', async (ctx, next) => {
+var signin = async (ctx, next) => {
     var
         name = ctx.request.body.name || '',
         password = ctx.request.body.password || '';
@@ -24,7 +24,7 @@ var signin = router.post('/signin', async (ctx, next) => {
         ctx.response.body = `<h1>Login failed!</h1>
         <p><a href="/">Try again</a></p>`;
     }
-});
+};
 
 module.exports = [
     {
