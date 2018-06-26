5de0a3442e0ccf127e01aa4936205b9b577c61ff
iromfeed <>
2018-06-26 19:32:53 +0800

    修改项目结构

--- koa/src/db/config.js
+++ koa/src/db/config.js
@@ -1,12 +1,11 @@
 import Sequelize from 'sequelize'
 
-const config = {
-    database: 'nodejs', // 使用哪个数据库
-    username: 'root', // 用户名
-    password: 'kxb13651478554', // 口令
-    host: '127.0.0.1', // 主机名
-    port: 3306 // 端口号，MySQL默认3306
-};
+let config = null;
+process.env.NODE_ENV === 'development' ?
+  config = require('./config-dev') :
+  process.env.NODE_ENV === 'uat' ?
+  config = require('./config-uat') :
+  config = require('./config-build');
 
 const sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
